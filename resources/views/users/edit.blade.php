bfa512c9f3b453f154fb9b74dbc86f16bffd94be
Nazwawa <hayakananazwa@gmail.com>
2022-03-18 11:49:09 +0700

    update

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -4,7 +4,7 @@
     <div class="row mt-5 mb-5">
         <div class="col-lg-12 margin-tb">
             <div class="float-left">
-                <h2>Edit Siswa</h2>
+                <h2>Edit Data</h2>
             </div>
             <div class="float-right">
                 <a class="btn btn-secondary" href="{{ route('users.index') }}"> Back</a>
